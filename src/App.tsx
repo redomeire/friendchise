b16e655a041723b7f68cd06b25dd9d80b1686226
Redomeire <redomeire2@gmail.com>
2023-02-14 22:02:58 +0700

    refactor: organize folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import React from "react";
 import { Route } from "react-router"
-import { Routes } from "react-router-dom"
-import Home from "./pages/Home"
+import { Routes, Outlet } from "react-router-dom"
+
+const Home = React.lazy(() => wait(1000).then(() => import('./pages/Home')))
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   )
+}
+
+const SuspenseWrapper = () => {
+  return (
+    <React.Suspense>
+      <Outlet />
+    </React.Suspense>
+  )
+}
+
+const wait = (time: number) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, time);
+  })
 }
 
 export default App
---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const getInitialValue = (key: string, initialValue: string | Function) => {
+    const value = window.localStorage.getItem(key);
+    if (value) return value
+    if (initialValue instanceof Function) return initialValue()
+
+    return initialValue
+}
+
+const useLocalStorage = (key: string, initialValue: string) => {
+    const [value, setValue] = React.useState(() => {
+        return getInitialValue(key, initialValue)
+    })
+
+    React.useEffect(() => {
+        window.localStorage.setItem(key, value)
+    }, [value])
+
+    return [value, setValue]
+}
+
+export default useLocalStorage
